f2e28a945172f95ccac72b654156f9518df2a423
JUANITOss <0juansuazo0@gmail.com>
2023-01-21 21:22:35 -0300

    completed ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let revString = "";
+    let stringCopy = string;
 
+    if (string === revString){
+        return string;
+    }
+    
+    for (let i = 0; i < string.length; i++) {
+        let revString = revString.concat(stringCopy.pop()); 
+    }
+
+    return revString;
 };
 
 // Do not edit below this line
