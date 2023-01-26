7e678405d3eb2404842c932881c923f0f69cb86f
JUANITOss <0juansuazo0@gmail.com>
2023-01-26 13:54:59 -0300

    Completed the 4th exersise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    args.forEach((arg) => {
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            if (element === arg) {
+                array.splice(i,1)
+            }
+        }
+    }
+    )
+
+    return array
 };
 
 // Do not edit below this line
