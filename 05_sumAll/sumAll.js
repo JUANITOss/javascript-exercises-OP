86e6454231db2cdbf4aaf54e447a9b84bed86bfd
JUANITOss <0juansuazo0@gmail.com>
2023-02-01 12:49:49 -0300

    Completed ex. 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
 
+    let totalSum = 0;
+
+    if (start > end) {
+
+        let aux = start
+        start = end
+        end = aux
+    }
+
+    if (Number(start) !== start || Number(end) !== end || start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+
+    for (let i = start; i < end+1; i++) {
+        totalSum = totalSum + i
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
